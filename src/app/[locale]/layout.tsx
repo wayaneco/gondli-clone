b370b9df622bb0018be55aaf0829c06c4fd9aa34
Sandro Domianidze <sdomianidze123@gmail.com>
2024-08-02 15:24:25 +0400

    feat: add google tag manager scripts

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata, Viewport } from 'next';
+import Script from 'next/script';
 import localFont from '@next/font/local';
 import { NextIntlClientProvider } from 'next-intl';
 import { getMessages } from 'next-intl/server';
@@ -58,12 +59,33 @@ export default async function RootLayout({
 
   return (
     <html lang={locale} className='scroll-smooth'>
+      <Script
+        id='gtm-script'
+        strategy='afterInteractive'
+        dangerouslySetInnerHTML={{
+          __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-PLM7GFXG');
+            `,
+        }}
+      />
       <body
         className={cn(
           'bg-surface-primary leading-snug sm:leading-none',
           gilroy.className,
         )}
       >
+        <noscript>
+          <iframe
+            src='https://www.googletagmanager.com/ns.html?id=GTM-PLM7GFXG'
+            height='0'
+            width='0'
+            className='invisible hidden'
+          ></iframe>
+        </noscript>
         <NextIntlClientProvider messages={messages}>
           {isComingSoon ? (
             <ComingSoon />
