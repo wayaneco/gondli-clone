34ffce14108cd3e59f219b4f7f94c262e24c7667
salim1999 <33951489+salim1999@users.noreply.github.com>
2024-10-26 15:54:00 +0200

    Update layout.tsx

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -34,33 +34,31 @@ export default async function RootLayout({
   const messages = await getMessages();
 
   return (
-    <html lang={locale} className='scroll-smooth'>
+    <html lang={locale} className="scroll-smooth">
       <Head>
-        <link
-          rel='icon'
-          href='https://blobgondlis.blob.core.windows.net/favicon/favicon.ico'
-        />
+        <link rel="icon" 
+          href="https://blobgondlis.blob.core.windows.net/favicon/favicon.ico" />
       </Head>
       <Script
-        id='gtm-script'
-        strategy='afterInteractive'
+        id="gtm-script"
+        strategy="afterInteractive"
         dangerouslySetInnerHTML={{
-          __html: 
+          __html: `
               (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
               new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
               j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
               'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
               })(window,document,'script','dataLayer','GTM-PLM7GFXG');
-            ,
+            `,
         }}
       />
       <body className={cn('bg-surface-primary leading-snug sm:leading-none')}>
         <noscript>
           <iframe
-            src='https://www.googletagmanager.com/ns.html?id=GTM-PLM7GFXG'
-            height='0'
-            width='0'
-            className='invisible hidden'
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PLM7GFXG"
+            height="0"
+            width="0"
+            className="invisible hidden"
           ></iframe>
         </noscript>
         <NextIntlClientProvider messages={messages}>
@@ -69,7 +67,7 @@ export default async function RootLayout({
           ) : (
             <>
               <HeaderSelector /> {/* Use HeaderSelector component */}
-              <div className='min-h-screen'>{children}</div>
+              <div className="min-h-screen">{children}</div>
               <Footer />
             </>
           )}
