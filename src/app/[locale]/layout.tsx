797db023ecd43ac0d6b97de87d4eb0e91e313298
salim1999 <33951489+salim1999@users.noreply.github.com>
2024-10-26 14:40:06 +0200

    Update layout.tsx

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -37,22 +37,21 @@ export default async function RootLayout({
     <html lang={locale} className='scroll-smooth'>
       <Head>
         <link
-  rel='icon'
-  href='https://blobgondlis.blob.core.windows.net/favicon/favicon.ico'
-/>
-
+          rel='icon'
+          href='https://blobgondlis.blob.core.windows.net/favicon/favicon.ico'
+        />
       </Head>
       <Script
         id='gtm-script'
         strategy='afterInteractive'
         dangerouslySetInnerHTML={{
-          __html: `
+          __html: 
               (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
               new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
               j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
               'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
               })(window,document,'script','dataLayer','GTM-PLM7GFXG');
-            `,
+            ,
         }}
       />
       <body className={cn('bg-surface-primary leading-snug sm:leading-none')}>
